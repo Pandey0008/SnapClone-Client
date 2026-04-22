2fef897c531209400814c6754dc495f71130b6c3
Your Name <your-email@example.com>
2026-04-22 10:46:05 +0530

    Axios API changed

--- src/services/storyService.js
+++ src/services/storyService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import API_BASE_URL from '../config/api'
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/v1/stories",
+  baseURL: `$${API_BASE_URL}/api/v1/stories`,
 });
 
 
