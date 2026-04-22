1a5d7579347b2990f2a3dcb64760ada771241fc8
Your Name <your-email@example.com>
2026-04-22 11:10:23 +0530

    $removed

--- src/services/storyService.js
+++ src/services/storyService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {API_BASE_URL} from '../config/api'
 
 const API = axios.create({
-  baseURL: `$${API_BASE_URL}/api/v1/stories`,
+  baseURL: `${API_BASE_URL}/api/v1/stories`,
 });
 
 
